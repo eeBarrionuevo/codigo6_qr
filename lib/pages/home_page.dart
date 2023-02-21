97a203521c67708d514f714a17dd90ef3a5be3c3
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2023-02-20 20:29:05 -0500

    first commit

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,84 @@
+import 'package:codigo6_qr/ui/general/colors.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              const Text(
+                "SaveQR",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24.0,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+              const SizedBox(
+                height: 12.0,
+              ),
+              const Text(
+                "Gestiona los códigos QR de tu preferencia y accede cuando quieras.",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.white70,
+                  fontSize: 16.0,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+              Image.asset(
+                "assets/images/qr.png",
+                width: width * 0.80,
+              ),
+              SizedBox(
+                width: double.infinity,
+                height: 48.0,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: Text(
+                    "Escanear QR",
+                    style: TextStyle(
+                      color: kBrandSecondaryColor,
+                      fontSize: 16.0,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: kBrandPrimaryColor,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(14.0),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 12.0,
+              ),
+              TextButton(
+                onPressed: () {},
+                child: Text(
+                  "Ver historial",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.w500,
+                    decoration: TextDecoration.underline,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
