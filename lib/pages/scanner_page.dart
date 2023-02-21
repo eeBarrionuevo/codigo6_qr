d2e62040a251c153b50fc5c189b00f8a7bdc3505
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2023-02-20 23:09:58 -0500

    Mods

--- lib/pages/scanner_page.dart
+++ lib/pages/scanner_page.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+import 'package:codigo6_qr/ui/general/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:qr_code_scanner/qr_code_scanner.dart';
 
@@ -10,6 +12,17 @@ class _ScannerPageState extends State<ScannerPage> {
   Barcode? result;
   QRViewController? controller;
   final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
+
+  @override
+  void reassemble() {
+    super.reassemble();
+    if (controller != null) {
+      if (Platform.isAndroid) {
+        controller!.pauseCamera();
+      }
+      controller!.resumeCamera();
+    }
+  }
 
   void _onPermissionSet(BuildContext context, QRViewController ctrl, bool p) {
     if (!p) {
@@ -26,15 +39,20 @@ class _ScannerPageState extends State<ScannerPage> {
     controller.scannedDataStream.listen((scanData) {
       setState(() {
         result = scanData;
+        if (result != null) {
+          print("XXXXXXXXXXXXXXXX: ${result!.code}");
+        }
       });
     });
+    controller.pauseCamera();
+    controller.resumeCamera();
   }
 
   Widget _buildQrView(BuildContext context) {
     // For this example we check how width or tall the device is and change the scanArea and overlay accordingly.
     var scanArea = (MediaQuery.of(context).size.width < 400 ||
             MediaQuery.of(context).size.height < 400)
-        ? 150.0
+        ? 300.0
         : 300.0;
     // To ensure the Scanner view is properly sizes after rotation
     // we need to listen for Flutter SizeChanged notification and update controller
@@ -42,13 +60,19 @@ class _ScannerPageState extends State<ScannerPage> {
       key: qrKey,
       onQRViewCreated: _onQRViewCreated,
       overlay: QrScannerOverlayShape(
-          borderColor: Colors.red,
-          borderRadius: 10,
-          borderLength: 30,
-          borderWidth: 10,
+          borderColor: kBrandPrimaryColor,
+          borderRadius: 16,
+          borderLength: 32,
+          borderWidth: 8,
           cutOutSize: scanArea),
       onPermissionSet: (ctrl, p) => _onPermissionSet(context, ctrl, p),
     );
+  }
+
+  @override
+  void dispose() {
+    controller?.dispose();
+    super.dispose();
   }
 
   @override
@@ -58,11 +82,49 @@ class _ScannerPageState extends State<ScannerPage> {
         children: [
           Expanded(
             flex: 4,
+            // child: _buildQrView(context),
             child: SizedBox(),
           ),
           Expanded(
             flex: 1,
-            child: SizedBox(),
+            child: Container(
+              width: double.infinity,
+              color: Colors.amber,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Por favor escanea un código QR.",
+                  ),
+                  SizedBox(
+                    width: double.infinity,
+                    height: 48.0,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => ScannerPage()));
+                      },
+                      child: Text(
+                        "Registra ahora",
+                        style: TextStyle(
+                          color: kBrandSecondaryColor,
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: kBrandPrimaryColor,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(14.0),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ),
         ],
       ),
