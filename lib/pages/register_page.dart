6b1b36ec1cd637f962a37f266627ca422ba5d7fb
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2023-02-21 21:34:18 -0500

    Mods

--- lib/pages/register_page.dart
+++ lib/pages/register_page.dart
@@ -1,6 +1,8 @@
+import 'package:codigo6_qr/ui/general/colors.dart';
 import 'package:codigo6_qr/ui/widgets/common_button_widget.dart';
 import 'package:codigo6_qr/ui/widgets/common_texfield_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:qr_flutter/qr_flutter.dart';
 
 class RegisterPage extends StatelessWidget {
   const RegisterPage({super.key});
@@ -8,52 +10,79 @@ class RegisterPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Column(
-            children: [
-              Text(
-                "Registrar contenido",
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 22.0,
-                  fontWeight: FontWeight.w700,
+      body: Stack(
+        children: [
+          SafeArea(
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Column(
+                  children: [
+                    Text(
+                      "Registrar contenido",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 22.0,
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 12.0,
+                    ),
+                    Text(
+                      "Por favor ingresa los campos requeridos.",
+                      style: TextStyle(
+                        color: Colors.white70,
+                        fontSize: 16.0,
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 30.0,
+                    ),
+                    CommonTextFieldWidget(
+                      hintText: "Ingresa un título...",
+                    ),
+                    SizedBox(
+                      height: 16.0,
+                    ),
+                    CommonTextFieldWidget(
+                      hintText: "Ingresa una observación...",
+                    ),
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    Container(
+                      padding: EdgeInsets.all(8.0),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(16.0),
+                      ),
+                      child: QrImage(
+                        data: "https://www.youtube.com/watch?v=ki_OcikiVRY",
+                        version: QrVersions.auto,
+                        size: 200.0,
+                      ),
+                    ),
+                    // Expanded(
+                    //   child: SizedBox(),
+                    // ),
+                  ],
                 ),
               ),
-              const SizedBox(
-                height: 12.0,
-              ),
-              Text(
-                "Por favor ingresa los campos requeridos.",
-                style: TextStyle(
-                  color: Colors.white70,
-                  fontSize: 16.0,
-                  fontWeight: FontWeight.w400,
-                ),
-              ),
-              const SizedBox(
-                height: 30.0,
-              ),
-              CommonTextFieldWidget(
-                hintText: "Ingresa un título...",
-              ),
-              SizedBox(
-                height: 16.0,
-              ),
-              CommonTextFieldWidget(
-                hintText: "Ingresa una observación...",
-              ),
-              SizedBox(
-                height: 20.0,
-              ),
-              CommonButtonWidget(
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: CommonButtonWidget(
                 onPressed: () {},
                 text: "Guardar",
               ),
-            ],
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
