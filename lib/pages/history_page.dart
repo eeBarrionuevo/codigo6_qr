9652562a43470bdb998e643396caaeb9b3dec081
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2023-02-23 19:30:45 -0500

    Mods

--- lib/pages/history_page.dart
+++ lib/pages/history_page.dart
@@ -32,42 +32,78 @@ class HistoryPage extends StatelessWidget {
                   ),
                 ),
                 ListView.builder(
+                  physics: const NeverScrollableScrollPhysics(),
                   shrinkWrap: true,
                   itemCount: 10,
                   itemBuilder: (BuildContext context, int index) {
                     return Container(
-                      color: Colors.white.withOpacity(0.09),
                       margin: const EdgeInsets.symmetric(vertical: 10.0),
                       padding: const EdgeInsets.symmetric(
                         horizontal: 12.0,
                         vertical: 14.0,
                       ),
+                      decoration: BoxDecoration(
+                        color: Colors.white.withOpacity(0.09),
+                        borderRadius: BorderRadius.circular(14.0),
+                      ),
                       child: Row(
                         children: [
-                          Column(
-                            children: [
-                              Text(
-                                "12-02-2022 12:30",
-                                style: TextStyle(
-                                  color: Colors.white54,
-                                  fontSize: 14.0,
+                          Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Row(
+                                  children: [
+                                    Icon(
+                                      Icons.calendar_month,
+                                      size: 14.0,
+                                      color: Colors.white54,
+                                    ),
+                                    Text(
+                                      " 12-02-2022 12:30",
+                                      style: TextStyle(
+                                        color: Colors.white54,
+                                        fontSize: 14.0,
+                                      ),
+                                    ),
+                                  ],
                                 ),
-                              ),
-                              Text(
-                                "Pasta Canteen",
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 16.0,
+                                const SizedBox(
+                                  height: 3,
                                 ),
-                              ),
-                              Text(
-                                "Carnes y Parrillas",
-                                style: TextStyle(
-                                  color: Colors.white54,
-                                  fontSize: 14.0,
+                                Text(
+                                  "Pasta Canteen",
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 16.0,
+                                  ),
                                 ),
-                              ),
-                            ],
+                                const SizedBox(
+                                  height: 3,
+                                ),
+                                Text(
+                                  "Carnes y Parrillas",
+                                  style: TextStyle(
+                                    color: Colors.white54,
+                                    fontSize: 14.0,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          IconButton(
+                            onPressed: () {},
+                            icon: Icon(
+                              Icons.link,
+                              color: Colors.white,
+                            ),
+                          ),
+                          IconButton(
+                            onPressed: () {},
+                            icon: Icon(
+                              Icons.qr_code,
+                              color: Colors.white,
+                            ),
                           ),
                         ],
                       ),
